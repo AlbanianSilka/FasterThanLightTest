441f5bf48d863ff75e3828589423d8596c3d4ad7
AlbanianSilka <andrey2096@gmail.com>
2023-03-05 20:33:35 +0200

    Added an ability to send expenses of user to another user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,15 @@
 class MessagesController < ApplicationController
   def create
-    @message = current_user.messages.create(body: msg_params[:body], room_id: params[:room_id])
+    if params[:select_msg]
+      @message = current_user.messages.create(body: msg_params[:body], room_id: params[:room_id])
+    elsif params[:select_exp]
+      current_user.expenses.each do |expense|
+        msg_expense = "Type: #{expense.expense_type}\n
+        Price: #{expense.price}\n
+        Name: #{expense.name}"
+        @message = current_user.messages.create(body: msg_expense, room_id: params[:room_id])
+      end
+    end
   end
 
   private
